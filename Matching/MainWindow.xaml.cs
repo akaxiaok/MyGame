351a2fcfc7630445128deb700dd675b16f3dbe5d
Kimi <akaxiaok@gmail.com>
2015-11-07 20:00:45 +0800

    matching complet

--- Matching/MainWindow.xaml.cs
+++ Matching/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -22,7 +23,8 @@ namespace Matching
         private const int Count = 16; //方块数量
         private readonly Tile[] _tiles = new Tile[Count]; //所有方块
         private int _score = 0;
-
+        public Tile TileSelect { set; get; }
+        public Tile TileCilck { set; get; }
         public MainWindow()
         {
             InitializeComponent();
@@ -33,23 +35,106 @@ namespace Matching
             Queue<int> quene = new Queue<int>();
             for (int i = 0; i < 4; i++)
             {
-               for (int j=1;j<5;j++)
-               {
-                   quene.Enqueue(j);
-               }
+                for (int j = 1; j < 5; j++)
+                {
+                    quene.Enqueue(j);
+                }
             }
             Random rand = new Random();
-           
+
             while (_tiles.Count(r => r.Value == 0) != 0)
             {
                 int n = _tiles.Count(r => r.Value == 0);
                 Tile t = _tiles[rand.Next(0, 16)];
-                if (t.Value==0)
+                if (t.Value == 0)
                 {
                     t.Value = quene.Dequeue();
                 }
-            };
-            string log = "2";
+            }
+        }
+
+        public void SetTileSelect(Tile tile)
+        {
+            TileSelect = tile;
+        }
+
+        public void SetTileClick(Tile tile)
+        {
+            TileCilck = tile;
+            tile.Print();
+        }
+
+        public void Process()
+        {
+            if (TileCilck == null && TileCilck == null)
+            {
+                return;
+            }
+
+            if (TileCilck.Value == TileSelect.Value)
+            {
+                var c = Color.FromArgb(255, 187, 173, 160);
+                TileCilck.Color = c;
+                TileSelect.Color = c;
+                TileCilck.Value = -1;
+                TileSelect.Value = -1;
+                TileSelect.Print(c);
+                TileCilck.Print(c);
+            }
+            else
+            {
+                Thread.Sleep(1000);
+                var c = Color.FromArgb(255, 204, 192, 178);
+                TileCilck.Color = c;
+                TileSelect.Color = c;
+                TileCilck.Print(c);
+                TileSelect.Print(c);
+            }
+            TileSelect = null;
+            TileCilck = null;
+        }
+
+        private void MainWin_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Process();
+        }
+
+        private void MainWin_Unloaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void MainWin_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var v in _tiles)
+            {
+                v.Value = 0;
+                var c = Color.FromArgb(255, 204, 192, 178);
+                v.Print(c);
+            }
+
+            Queue<int> quene = new Queue<int>();
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 1; j < 5; j++)
+                {
+                    quene.Enqueue(j);
+                }
+            }
+            Random rand = new Random();
+
+            while (_tiles.Count(r => r.Value == 0) != 0)
+            {
+                int n = _tiles.Count(r => r.Value == 0);
+                Tile t = _tiles[rand.Next(0, 16)];
+                if (t.Value == 0)
+                {
+                    t.Value = quene.Dequeue();
+                }
+            }
         }
     }
 }
--- Matching/Tile.xaml.cs
+++ Matching/Tile.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,8 +20,9 @@ namespace Matching
     /// </summary>
     public partial class Tile : UserControl
     {
-        public Color Color { get; private set; }
+        public Color Color { get; set; }
         public int Value { get; set; }
+        private MainWindow win;
         public Tile()
         {
             InitializeComponent();
@@ -28,8 +30,118 @@ namespace Matching
 
         public void SetColor(Color color)
         {
+
             MainArea.Background = new SolidColorBrush(color);
             Color = color;
+
         }
+
+        private void MainArea_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+            //if (sel.Value == this.Value)
+            //{
+
+            //    sel.Color = Color.FromArgb(255, 187, 173, 160);
+            //    this.Color = Color.FromArgb(255, 187, 173, 160);
+            //    sel.Value = -1;
+            //    this.Value = -1;
+            //    Print(sel);
+            //    Print(this);
+            //}
+            //else
+            //{
+            //    Thread.Sleep(1000);
+            //    sel.Color = Color.FromArgb(255, 204, 192, 178);
+            //    this.Color = Color.FromArgb(255, 204, 192, 178);
+            //    Print(sel);
+            //    Print(this);
+            //}
+            //SetTileSelect();
+        }
+
+        public void Print(Color col)
+        {
+            this.SetColor(col);
+        }
+        public void Print()
+        {
+            switch (Value)
+            {
+                case 1:
+                    SetColor(Colors.BlueViolet);
+                    break;
+                case 2:
+                    SetColor(Colors.Crimson);
+                    break;
+                case 3:
+                    SetColor(Colors.DarkSlateGray);
+                    break;
+                case 4:
+                    SetColor(Colors.MediumSeaGreen);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void SetTileSelect()
+        {
+            win.SetTileSelect(this);//主窗口MaiWin处理一次点击事件
+        }
+
+        public void SetTileClick()
+        {
+            win.SetTileClick(this);
+        }
+        /// <summary>
+        /// 通过name找父控件
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj">源控件</param>
+        /// <param name="name">父控件的name</param>
+        /// <returns></returns>
+        private T GetParentObject<T>(DependencyObject obj, string name) where T : FrameworkElement
+        {
+            DependencyObject parent = VisualTreeHelper.GetParent(obj);
+
+            while (parent != null)
+            {
+                if (parent is T && (((T)parent).Name == name | string.IsNullOrEmpty(name)))
+                {
+                    return (T)parent;
+                }
+
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+
+            return null;
+        }
+
+        private void UserControl_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            win = GetParentObject<MainWindow>(this, "MainWin");//通过name找父控件
+            var sel = win.TileSelect;
+            if (this.Value == -1)
+            {
+                return;
+            }
+            if (this.Equals(win.TileSelect))
+            {
+                return;
+            }
+            if (sel == null || sel.Value <= 0)
+            {
+                SetTileSelect();
+                Print();
+            }
+            else
+            {
+                Print();
+                SetTileClick();
+            }
+        }
+
+
     }
 }
